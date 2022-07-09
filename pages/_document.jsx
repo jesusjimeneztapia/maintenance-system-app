4b32f2af287d45d63f8cfccc12c7eedf612fd9e6
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2022-07-09 17:58:14 -0400

    Create project

---
+++ pages/_document.jsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function MyDocument() {
+  return (
+    <Html lang='es'>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>TECNOPOR S.A.</title>
+      </Head>
+      <h1>TECNOPOR S.A.</h1>
+      <h4>Sistema de mantenimiento</h4>
+    </>
+  )
+}
